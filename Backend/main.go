a43d36e9477cdc1fd622b99c17e0156434793130
ping-yee <611077101@mail.nknu.edu.tw>
2023-09-04 16:57:25 +0800

    Dev: Backend project initial with Gin.

--- Backend/main.go
+++ Backend/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-    fmt.Printf("Hello, world or 你好，世界 or καλημ ́ρα κóσμ or こんにちはせかい\n")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
