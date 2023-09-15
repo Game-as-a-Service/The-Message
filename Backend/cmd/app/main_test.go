c9d7301868f577a7e292f029d2385116938cf8e1
ping-yee <611077101@mail.nknu.edu.tw>
2023-09-15 13:13:39 +0800

    feat: The e2e tests have been implemented.

--- Backend/cmd/app/main_test.go
+++ Backend/cmd/app/main_test.go
@@ -9,6 +9,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"regexp"
+	"strings"
 	"testing"
 
 	"gorm.io/driver/mysql"
@@ -22,14 +24,14 @@ import (
 )
 
 func TestGetGameByIdE2E(t *testing.T) {
-	// 創建一個虛擬的MySQL數據庫連接
+	// Create a virtual MySQL database connection
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("Error creating mock database: %v", err)
 	}
 	defer db.Close()
 
-	// 替換GORM的數據庫連接為虛擬的連接
+	// Replace the GORM database connection with the virtual one
 	gdb, err := gorm.Open(mysql.New(mysql.Config{
 		Conn:                      db,
 		SkipInitializeWithVersion: true,
@@ -38,89 +40,89 @@ func TestGetGameByIdE2E(t *testing.T) {
 		t.Fatalf("Error opening GORM database: %v", err)
 	}
 
-	// 設置預期的模擬數據庫查詢和操作
-	mock.ExpectQuery("SELECT `id`,`name` FROM `games` WHERE id = ? AND `games`.`deleted_at` IS NULL ORDER BY `games`.`id` LIMIT 1").
+	// Set up expected mock database queries and operations
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT `id`,`name` FROM `games` WHERE id = ? AND `games`.`deleted_at` IS NULL ORDER BY `games`.`id` LIMIT 1")).
 		WithArgs(1).
 		WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Test Game"))
 
-	// 創建Gin路由和HTTP伺服器
+	// Create a Gin router and HTTP server
 	router := gin.Default()
 	gameHandler := &httpHandler.Game{
 		GameRepo: mysqlRepo.NewGameRepositoryRepository(gdb),
 	}
 	router.GET("/api/v1/game/:gameId", gameHandler.GetGameById)
 
-	// 準備HTTP GET請求
+	// Prepare an HTTP GET request
 	req, _ := http.NewRequest("GET", "/api/v1/game/1", nil)
 	recorder := httptest.NewRecorder()
 
-	// 執行HTTP請求
+	// Execute the HTTP request
 	router.ServeHTTP(recorder, req)
 
-	// 檢查HTTP響應
+	// Check the HTTP response
 	assert.Equal(t, http.StatusOK, recorder.Code)
 
-	// 解析HTTP響應
+	// Parse the HTTP response
 	var response mysqlRepo.Game
 	err = json.Unmarshal(recorder.Body.Bytes(), &response)
 	assert.NoError(t, err)
 
-	// 檢查模擬的數據庫調用是否符合預期
+	// Check if the mock database expectations were met
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Errorf("Unfulfilled expectations: %s", err)
 	}
 
-	// 檢查數據庫操作是否正確，這可以通過模擬數據庫操作來實現
+	// Check if the database operations were correct; this is achieved through mocking
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %s", err)
+	}
 }
 
-// func TestCreateGameE2E(t *testing.T) {
-// 	// 創建一個虛擬的MySQL數據庫連接
-// 	db, mock, err := sqlmock.New()
-// 	if err != nil {
-// 		t.Fatalf("Error creating mock database: %v", err)
-// 	}
-// 	defer db.Close()
+func TestCreateGameE2E(t *testing.T) {
+	// Create a virtual MySQL database connection
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Error creating mock database: %v", err)
+	}
+	defer db.Close()
 
-// 	// 設置預期的模擬數據庫查詢和操作
-// 	mock.ExpectExec("INSERT INTO games").WillReturnResult(sqlmock.NewResult(1, 1))
+	// Replace the GORM database connection with the virtual one
+	gdb, err := gorm.Open(mysql.New(mysql.Config{
+		Conn:                      db,
+		SkipInitializeWithVersion: true,
+	}), &gorm.Config{})
+	if err != nil {
+		t.Fatalf("Error opening GORM database: %v", err)
+	}
 
-// 	// 初始化GORM數據庫連接
-// 	gdb, err := gorm.Open(mysql.New(mysql.Config{
-// 		Conn:                      db,
-// 		DriverName:                "mysql",
-// 		SkipInitializeWithVersion: true,
-// 	}), &gorm.Config{})
+	// Set up expected mock database queries and operations for CreateGame
+	mock.ExpectBegin() // Expect a transaction Begin
+	mock.ExpectExec("INSERT INTO `games`").WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit() // Expect a transaction Commit
 
-// 	if err != nil {
-// 		t.Fatalf("Error opening GORM database: %v", err)
-// 	}
+	// Create a Gin router and HTTP handler
+	router := gin.Default()
+	gameHandler := &httpHandler.Game{
+		GameRepo: mysqlRepo.NewGameRepositoryRepository(gdb),
+	}
+	router.POST("/api/v1/game", gameHandler.CreateGame) // Register the CreateGame route
 
-// 	// 創建Gin路由和HTTP伺服器
-// 	router := gin.Default()
-// 	gameHandler := &httpHandler.Game{
-// 		GameRepo: mysqlRepo.NewGameRepositoryRepository(gdb),
-// 	}
-// 	router.POST("/api/v1/game", gameHandler.CreateGame)
+	// Prepare an HTTP POST request to create a game
+	createGameRequest := `{"name": "Test Game"}`
+	req, _ := http.NewRequest("POST", "/api/v1/game", strings.NewReader(createGameRequest))
+	req.Header.Set("Content-Type", "application/json")
+	recorder := httptest.NewRecorder()
 
-// 	// 準備HTTP POST請求
-// 	reqBody := []byte(`{ "name": "Test Game" }`)
-// 	req, _ := http.NewRequest("POST", "/api/v1/game", bytes.NewBuffer(reqBody))
-// 	req.Header.Set("Content-Type", "application/json")
-// 	recorder := httptest.NewRecorder()
+	// Execute the HTTP request
+	router.ServeHTTP(recorder, req)
 
-// 	// 執行HTTP請求
-// 	router.ServeHTTP(recorder, req)
+	// Check the HTTP response status code
+	if recorder.Code != http.StatusOK {
+		t.Fatalf("Expected status code 200, got %d. Response Body: %s", recorder.Code, recorder.Body.String())
+	}
 
-// 	// 檢查HTTP響應
-// 	assert.Equal(t, http.StatusOK, recorder.Code)
-
-// 	// 解析HTTP響應
-// 	var response mysqlRepo.Game
-// 	err = json.Unmarshal(recorder.Body.Bytes(), &response)
-// 	assert.NoError(t, err)
-
-// 	// 檢查模擬的數據庫調用是否符合預期
-// 	// if err := mock.ExpectationsWereMet(); err != nil {
-// 	// 	t.Errorf("Unfulfilled expectations: %s", err)
-// 	// }
-// }
+	// Check if the mock database expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %s", err)
+	}
+}
